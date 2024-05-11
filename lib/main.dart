8c4b0b8fe21d2b8e0b90c90bc2afbccb94ee0bce
Raghav-995 <raghavendrapathak0@gmail.com>
2024-05-11 11:19:31 +0530

    adding app bar

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:openaiflutter/myapp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/myapp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('OpenAI with Flutter'),
+        ),
+      ),
+    );
+  }
+}
